151693e0bfcf777ce6a9c294edb7106877041dc1
Sonali Nimse T_36 <sonalasane83@gmail.com>
2023-02-14 08:44:54 +0530

    virsion_3

---
+++ Transfer Statements.py
@@ -0,0 +1,47 @@
+# Transfer Statements
+
+# break,continue,pass
+
+#break:-
+'''
+we can use break statement inside loop to break loop execution based on some condition
+'''
+
+'''for i in range(1,11):
+    if i == 7:
+        print("Please break after 6, we don't need")
+        break
+    print(i)'''
+
+#continue
+
+'''We can use continue statement to skip current and continue nect iteration'''
+
+#odd no.
+
+'''for i in range(1,11):
+    if i %2 == 0:
+        continue
+    print(i)'''
+
+#pass
+'''
+pass is a keyword
+
+in our programming syntactically if block is required which won't do anything then we can define that
+empty block with pass keyword.
+
+empty block:-
+IndentationError: expected an indented block afte
+
+-> It is an empty statement
+-> Null statement
+-> a statement won't do anything
+
+
+'''
+if True:
+    pass
+
+
+
